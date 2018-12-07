d98134aa7c79fc3a4b6f0cd467cb3e2718ef2b29
Gohikegirl <gohikegirl@gmail.com>
2018-12-06 18:50:25 -0600

    Add Insurance Project

---
+++ Insurace Project/Insurace Project/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Insurace_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI(Yes/No)?");
+            string dui = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            bool result = (age > 15 && dui == "No" && tickets < 4);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
