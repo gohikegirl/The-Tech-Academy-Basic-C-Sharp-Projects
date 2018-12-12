3974c604138a0d0afb34504fb2edf67f7617c161
Gohikegirl <gohikegirl@gmail.com>
2018-12-12 07:19:37 -0800

    Add loop exercises (2)

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the temperature? (Enter in degrees Fahrenheit)");
+            int temperature = Convert.ToInt32(Console.ReadLine());
+            bool adjustNeeded = temperature == 60;
+
+            switch (temperature)
+            {
+                case (55):
+                    Console.WriteLine("Can you turn up the heater?");
+                    Console.WriteLine("What is the temperature? (Enter in degrees Fahrenheit)");
+                    temperature = Convert.ToInt32(Console.ReadLine());
+                    break;
+                case (59):
+                    Console.WriteLine("Can you turn up the heater?");
+                    Console.WriteLine("What is the temperature? (Enter in degrees Fahrenheit)");
+                    temperature = Convert.ToInt32(Console.ReadLine());
+                    break;
+                case (65):
+                    Console.WriteLine("Can you turn down the heater?");
+                    Console.WriteLine("What is the temperature? (Enter in degrees Fahrenheit)");
+                    temperature = Convert.ToInt32(Console.ReadLine()); ;
+                    break;
+                case (60):
+                    Console.WriteLine("Wow!It is the perfect temp for sleeping.");
+                    adjustNeeded = true;
+                    break;
+                default:
+                    Console.WriteLine("I think we need to adjust the heater.");
+                    Console.WriteLine("What is the temperature? (Enter in degrees Fahrenheit)");
+                    temperature = Convert.ToInt32(Console.ReadLine());
+                    break;
+            }
+            while (!adjustNeeded) ;
+           
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LoopExercise1/LoopExercise1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If you're happy and you know it clap your hands. (write 'clap' until you're sick of it)");
+            string happyOrNot = Console.ReadLine();
+            bool NotYetSickOfIt = happyOrNot == "clap";
+
+            while (NotYetSickOfIt == true)
+            {
+                switch (happyOrNot)
+                {
+                    case "clap":
+                        Console.WriteLine("If you're happy and you know it clap your hands. (write 'clap' until you're sick of it)");
+                        happyOrNot = Console.ReadLine();
+                        break;
+
+                    default:
+                        Console.WriteLine("Sorry to hear you're sick of it. We will stop asking now.");
+                        NotYetSickOfIt = false;
+                        break;
+                                       
+                 }
+            }
+            Console.ReadLine();
+
+     
+        }
+    }
+}
