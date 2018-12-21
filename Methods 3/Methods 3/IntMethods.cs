ea57f432bc806b237b9f89e96911c0bb1bb0616d
Gohikegirl <gohikegirl@gmail.com>
2018-12-21 15:11:21 -0800

    Add Methods 3 Project

---
+++ Methods 3/Methods 3/IntMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_3
+{
+    class IntMethods
+    {
+        public static int Method (int i)
+        {
+            return i * i;
+        }
+
+        public static int Method (decimal d)
+        {
+            decimal mResult = d + d;
+            return Convert.ToInt32(mResult);
+           
+        }
+
+        public static int Method (string s)
+        {
+            int cResult = Convert.ToInt32(s);
+            return cResult*cResult;
+
+        }
+    }
+}
---
+++ Methods 3/Methods 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Steps 1 and 2 in exercise
+            Console.WriteLine(IntMethods.Method(i: 4));
+
+            //Steps 3 and 4
+            decimal Input = Convert.ToDecimal(4.34);
+            Console.WriteLine(IntMethods.Method(d: Input));
+           
+
+            //Steps 5 and 6
+            Console.WriteLine(IntMethods.Method("3"));
+            Console.ReadLine();
+        }
+    }
+}
