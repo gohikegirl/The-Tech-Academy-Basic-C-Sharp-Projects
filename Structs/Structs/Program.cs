0078346e56689bec91287545ade340dfdb18ce0d
Gohikegirl <gohikegirl@gmail.com>
2018-12-26 19:56:53 -0800

    Add Structs Project

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 10.678m;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ TwentyOne/TwentyOne/Notes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Notes
+    {
+        //enums are a data type that has a fixed set of options--e.g., days of the week--that one is trying to keep the possible instances of that data limited to
+        //can name them within a class or in a main program
+        //if limiting user inputs (in string form) to an enum, must use the ([enter enum])Enum.Parse(typeof(enter enum), [enter string variable])
+        
+        //classes vs structs: structs are pretty much like classes except that they contain values not references
+        //structs are good to use instead of classes when the value of an item in the struct/class there is not going to change ever
+        //structs are not "nullable"--they must have a value
+        //examples of structs are integers, boolean,
+        //strings are always values
+        
+    }
+}
